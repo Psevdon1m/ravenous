2e46a85f65efd53409a0913e54ec68edf897f0a1
Valentyn <valentine.oleynik@gmail.com>
2020-05-03 21:51:35 +0300

    Updates

---
+++ precache-manifest.5aa1f1cfd12d0c36278f4f300306d87e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb26a0adb7b2094a164701a8f463cbc8",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "57e65138a6bdf5945376",
+    "url": "/ravenous/static/css/main.790abec9.chunk.css"
+  },
+  {
+    "revision": "231fde5d0438c75fce7b",
+    "url": "/ravenous/static/js/2.60e7050c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous/static/js/2.60e7050c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57e65138a6bdf5945376",
+    "url": "/ravenous/static/js/main.86032878.chunk.js"
+  },
+  {
+    "revision": "f3bd2525e07e09724bd8",
+    "url": "/ravenous/static/js/runtime-main.329af534.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "83942fdc46cb8b6a22ca6f6bd2b1e428",
+    "url": "/ravenous/static/media/ravenous_favicon.83942fdc.ico"
+  }
+]);
